2fe5da3623ce3bf074b7157b5808052cb1be00fe
Tiffany_Tam <tamtiffany89@gmail.com>
2021-10-14 08:53:32 -0400

    commit tree 96aad44f277f50b5c53565d495a81634b06c71c5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function saturdayFun (activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork (activity = 'go to the office') {
+    return(`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective (work = "*") {
+    return function innerFunction(adj = "special") {
+        return (`You are ${work}${adj}${work}!`)
+        // return (`You are ${hl} ${adj} ${hl}!`) 
+    }
+wrapAdjective();
+}
+   
